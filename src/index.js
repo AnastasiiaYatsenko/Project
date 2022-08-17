8719c5165bc2801725109d37cacd744b25fe56a1
AnastasiiaYatsenko <14nastiayatsenko@gmail.com>
2022-08-17 19:00:31 +0300

    Added 5-day forecast via API

--- src/index.js
+++ src/index.js
@@ -17,6 +17,13 @@ if (minutes < 10) {
 currentDate = document.querySelector("#current-time");
 currentDate.innerHTML = `${date} ${month}, ${today} ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return daysOfWeek[day];
+
+}
 
 function searchCity(city) {
     let apiKey = "486d80106caa3182b4a0eb80c7778807"; 
@@ -32,8 +39,9 @@ function searchTown(event) {
    let apiKey = "486d80106caa3182b4a0eb80c7778807"; 
    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
    axios.get(apiUrl).then(showWeather);
-
 }
+
+
  
 
 function showWeather(response) {
@@ -48,12 +56,50 @@ function showWeather(response) {
     let icon = document.querySelector("#icon");
     icon.setAttribute('src', `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     icon.setAttribute('alt', `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`);
+
+    displayForecast(response.data.coord);
 }  
+
+function showForecast(response) {
+    console.log(response.data.daily);
+   let forecast = response.data.daily;
+   let forecastElement = document.querySelector("#forecast");
+   
+   let forecastHTML = `<div class="row">`;
+   forecast.forEach(function (forecastDay, index) {
+    if (index < 6 && index >= 1) {
+    forecastHTML = forecastHTML + 
+    `
+    <div class="col">
+        <div class="card">
+            <div class="card-body forecast-box">
+            <h6 class="day">${formatDay(forecastDay.dt)}</h6>
+            <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" class="icon" id="icon"> 
+            <span id="weather-forecast-temp-max">${Math.round(forecastDay.temp.max)}</span>° |
+            <span id="weather-forecast-temp-min">${Math.round(forecastDay.temp.min)}</span>°
+            </div>
+        </div>
+    </div>`
+    }
+   });
+   
+   forecastHTML = forecastHTML + `</div>`;
+   forecastElement.innerHTML = forecastHTML;
+}
+
+function displayForecast(coord) {
+    let apiKey = "486d80106caa3182b4a0eb80c7778807";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showForecast);
+}
 
 let searchButton = document.querySelector("#search-form");
 searchButton.addEventListener("submit", searchTown);
 
 searchCity("Kyiv");
+
+
+   
 
 
 
